f4d8d9a2c4595621fa3417521b873d00bfd512c8
Tomasz Kalinowski <tomasz.kalinowski@tooploox.com>
2021-08-10 16:21:23 +0200

    pure html static test

--- server/index.js
+++ server/index.js
@@ -5,11 +5,8 @@ const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "../client", "build")));
-app.use(express.static(path.join(__dirname, "../client/public/")));
-
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/search/:phrase", async function (req, res) {
