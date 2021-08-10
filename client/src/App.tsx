9c8ea87ddae9d554311aa2503c166d357dab0759
Tomasz Kalinowski <tomasz.kalinowski@tooploox.com>
2021-08-10 16:47:01 +0200

    add styled components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TestWrapper>Styled components test</TestWrapper>;
 }
 
 export default App;
+
+const TestWrapper = styled.div`
+  background-color: red;
+  height: 100px;
+  width: 100px;
+`;
